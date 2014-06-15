3349b8592eb1d08c5f5d255b53018b0fd04beb3f
Junhao HUA <huajh7@gmail.com>
2014-06-15 16:21:31 +0800

    svr

--- main.m
+++ main.m
@@ -3,8 +3,11 @@ if_hist_age = 0;
 if_partition_dataset = 0;
 if_knntrain = 0;
 if_knntest = 0;
-if_svmtrain = 1;
+if_svmtrain =0;
 if_svmtest = 1;
+if_svm_mixed = 0;
+
+addpath funtions;
 
 if if_hist_age == 1
     load('Allfeatures.mat');
@@ -66,60 +69,84 @@ if if_knntest == 1
         pred_age(i) = idx+minage-1;
     end    
     error = abs(pred_age-testset.age);
-    mean_abs_error = mean(error);
-    fprintf('mean_abs_error: %.2f\n',mean_abs_error);
-    hist(error,maxage-minage+1);    
+    MAE = mean(error);
+    fprintf('mean_abs_error: %.2f\n',MAE);
+    hist(error,20);  
+    title(['MAE = ',num2str(MAE)]);
+    save('knnError.mat','error','MAE');
 end
 
-if if_svmtrain == 1
-    train_num = length(trainset.features);
-    % 0  - 20
-    % 21 - 50
-    % 50 - 
-    [~,child_idx] = find(trainset.age<=20); 
-    [~,adult_idx] = find(trainset.age>20 & trainset.age<=50 ); 
-    [~,old_idx]   = find(trainset.age>50);
-    trainIDX = ones(train_num,1);
-    trainIDX(adult_idx) = 2;
-    trainIDX(old_idx) = 3;
-    
-    % classifcation: chlid, adult, old
-    classify_svm = svmtrain(trainIDX,trainset.features,'-t 2');
-    
-    %regresssion    
-    child_svm = svmtrain(trainset.age(child_idx),trainset.age(child_idx,:),'-s 3 -t 2');
-    adult_svm = svmtrain(trainset.age(adult_idx),trainset.age(adult_idx,:),'-s 3 -t 2');
-    old_svm   = svmtrain(trainset.age(old_idx),trainset.age(old_idx,:),'-s 3 -t 2');    
-    save('svm_model.mat','classify_svm','child_svm','adult_svm','old_svm');
+if if_svmtrain == 1    
+    trainfeatures = trainset.features(1:5:end,:);
+    trainage = trainset.age(1:5:end)';
+    train_num = length(trainage);
+    if if_svm_mixed == 1
+        % 0  - 30
+        % 31 - 45
+        % 46 - 
+        [~,child_idx] = find(trainage'<=30); 
+        [~,adult_idx] = find(trainage'>30 & trainage'<=45 ); 
+        [~,old_idx]   = find(trainage'>45);
+        trainIDX = ones(train_num,1);
+        trainIDX(adult_idx) = 2;
+        trainIDX(old_idx) = 3;
+
+        % classifcation: chlid, adult, old
+        classify_svm = svmtrain(trainIDX,trainfeatures,'-t 2');
+
+        %regresssion    
+        child_svr = svmtrain(trainage(child_idx),trainfeatures(child_idx,:),'-s 3 -t 2');
+        adult_svr = svmtrain(trainage(adult_idx),trainfeatures(adult_idx,:),'-s 3 -t 2');
+        old_svr   = svmtrain(trainage(old_idx),trainfeatures(old_idx,:),'-s 3 -t 2');    
+        save('svm_model.mat','classify_svm','child_svr','adult_svr','old_svr');
+    else
+        tic
+        svr_model = svmtrain(trainage,trainfeatures,'-s 3 -t 2 -h 0');
+        toc
+        save('svr_rbf_model.mat','svr_model');
+    end
 else 
-    load('svm_model.mat');
+    load('svr_rbf_model.mat');
+    %load('svr_poly_model.mat');
 end
 
 if if_svmtest == 1
-    test_num = length(testset.features);
+    testfeatures = testset.features(1:end,:);
+    testage = testset.age(1:end)';    
+    test_num = length(testage);    
+    if if_svm_mixed ==1
+        [~,child_idx] = find(testage'<=30); 
+        [~,adult_idx] = find(testage'>30 & testage'<=45); 
+        [~,old_idx]   = find(testage'>45);
+        testIDX = ones(test_num,1);
+        testIDX(adult_idx) = 2;
+        testIDX(old_idx) = 3;
+
+        which_part = svmpredict(testIDX,testfeatures,classify_svm);    
+        [~,child_idx] = find(which_part==1);
+        [~,adult_idx] = find(which_part==2);
+        [~,old_idx] = find(which_part==3);
+
+        pred_age = ones(test_num,1);
+        age1 =svmpredict(testage(child_idx),testfeatures(child_idx,:),child_svr); 
+        age2 =svmpredict(testage(adult_idx),testfeatures(adult_idx,:),adult_svr); 
+        age3 =svmpredict(testage(old_idx),testfeatures(old_idx,:),old_svr);       
+        pred_age(child_idx) = age1;
+        pred_age(adult_idx) = age2;
+        pred_age(old_idx)   = age3;
+    else     
+        tic;
+        pred_age =svmpredict(testage,testfeatures,svr_model); 
+        toc;
+    end
     
-    [~,child_idx] = find(testset.age<=20); 
-    [~,adult_idx] = find(testset.age>20 && testset.age<=50); 
-    [~,old_idx]   = find(testset.age>50);
-    testIDX = ones(test_num,1);
-    testIDX(adult_idx) = 2;
-    testIDX(old_idx) = 3;
-    
-    which_part = svmpredict(testIDX,testset.features,classify_svm);    
-    [~,child_idx] = find(which_part==1);
-    [~,adult_idx] = find(which_part==2);
-    [~,old_idx] = find(which_part==3);
-    
-    pred_age = ones(test_num,1);
-    pred_age(child_idx) =svmpredict(testset.age(child_idx),testset.features,child_svm); 
-    pred_age(adult_idx) =svmpredict(testset.age(adult_idx),testset.features,adult_svm); 
-    pred_age(old_idx) =svmpredict(testset.age(old_idx),testset.features,old_svm);             
-    
-    error = abs(pred_age-testset.age);
-    mean_abs_error = mean(error);
-    fprintf('mean_abs_error: %.2f\n',mean_abs_error);
-    hist(error);
-    
+    error = abs(pred_age-testage);
+    MAE = mean(error);
+    fprintf('mean_abs_error: %.2f\n',MAE);
+    hist(error,20);
+    title(['MAE = ',num2str(MAE)]);
+   % save('svr_poly_Error.mat','error','MAE');
+    save('svr_rbf_Error.mat','error','MAE');
 end
 
 
